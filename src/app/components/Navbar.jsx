859e90b8997b7d63bd537a865ea075cb81359458
Matthew Nweke <nwekematthew243@gmail.com>
2024-07-10 16:34:25 +0100

    navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
@@ -47,7 +47,7 @@ const Navbar = () => {
     >
       <div className="flex justify-between items-center h-[15vh]  px-5 ">
         <Link href="/">
-          <div className="relative  h-[10rem] w-[10rem]">
+          <div className="relative h-[10rem] w-[10rem]">
             <Image
               src="/logo.svg"
               className="cursor-pointer"
@@ -68,7 +68,7 @@ const Navbar = () => {
           >
             <Image
               src="/menu-bar.svg"
-              className="cursor-pointer "
+              className="cursor-pointer"
               loading="lazy"
               alt="Logo"
               layout="fill"
@@ -79,90 +79,88 @@ const Navbar = () => {
       </div>
 
       {/* Dropdown Menu */}
-      {isMenuOpen && (
-        <div
-          className={`menu lg:hidden fixed top-0 py-28 right-0 bottom-0 left-0 bg-[white] z-50 flex justify-center `}
-        >
-          <div className="absolute top-9 right-5 ">
-            <button
-              onClick={handleCloseMenu}
-              className="text-gray-600 relative w-7 h-7 hover:text-gray-800 focus:outline-none"
-            >
-              <Image
-                src="/close-circle.svg"
-                className="cursor-pointer "
-                loading="lazy"
-                alt="Logo"
-                layout="fill"
+      <div
+        className={`menu lg:hidden fixed top-0 bottom-0 right-0 w-full md:w-[60%] bg-white z-50 transform transition-transform duration-300 ${
+          isMenuOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div className="absolute top-9 right-5">
+          <button
+            onClick={handleCloseMenu}
+            className="text-gray-600 relative w-7 h-7 hover:text-gray-800 focus:outline-none"
+          >
+            <Image
+              src="/close-circle.svg"
+              className="cursor-pointer"
+              loading="lazy"
+              alt="Close"
+              layout="fill"
               objectFit="contain"
-
-              />
-            </button>
-          </div>
-          <div className="flex flex-col items-center gap-5 ">
-            <Link href="/">
-              <p className="px-5 py-3 text-center font-semibold max-md:w-[60vw] w-[50vw] rounded-3xl  text-white bg-[#0032F0] text-sm cursor-pointer max-lg:text-[1rem]">
-                Home
-              </p>
-            </Link>
-            <Link href="#">
-              <p className="px-5 py-3 text-center text-[#1A2244] max-md:w-[60vw] w-[50vw] rounded-3xl text-sm max-lg:text-[1rem] font-semibold">
-                Projects
-              </p>
-            </Link>
-            <Link href="#">
-              <p className="px-5 py-3 rounded-3xl text-center text-[#1A2244] max-md:w-[60vw] w-[50vw] text-sm max-lg:text-[1rem] font-semibold">
-                Contact
-              </p>
-            </Link>
-            <Link href="#">
-              <p className="px-5 py-3 rounded-3xl text-center text-[#1A2244] max-md:w-[60vw] w-[50vw] text-sm max-lg:text-[1rem] font-semibold">
-                About
-              </p>
-            </Link>
-            <Link href="#">
-              <p className="px-5 py-3  rounded-3xl text-center text-[#1A2244] max-md:w-[60vw] w-[50vw] text-sm max-lg:text-[1rem] font-semibold">
-                FAQ
-              </p>
-            </Link>
-          </div>
+            />
+          </button>
         </div>
-      )}
+        <div className="flex flex-col items-center gap-5 pt-28">
+          <Link href="/">
+            <p className="px-5 py-3 text-center font-semibold max-md:w-[60vw] w-[50vw] rounded-3xl text-white bg-[#0032F0] text-sm cursor-pointer max-lg:text-[1rem]">
+              Home
+            </p>
+          </Link>
+          <Link href="#">
+            <p className="px-5 py-3 text-center text-[#1A2244] max-md:w-[60vw] w-[50vw] rounded-3xl text-sm max-lg:text-[1rem] font-semibold">
+              Projects
+            </p>
+          </Link>
+          <Link href="#">
+            <p className="px-5 py-3 text-center text-[#1A2244] max-md:w-[60vw] w-[50vw] rounded-3xl text-sm max-lg:text-[1rem] font-semibold">
+              Contact
+            </p>
+          </Link>
+          <Link href="#">
+            <p className="px-5 py-3 text-center text-[#1A2244] max-md:w-[60vw] w-[50vw] rounded-3xl text-sm max-lg:text-[1rem] font-semibold">
+              About
+            </p>
+          </Link>
+          <Link href="#">
+            <p className="px-5 py-3 text-center text-[#1A2244] max-md:w-[60vw] w-[50vw] rounded-3xl text-sm max-lg:text-[1rem] font-semibold">
+              FAQ
+            </p>
+          </Link>
+        </div>
+      </div>
 
       {/* Navigation Links */}
-      <div className={`lg:flex items-center  gap-10 hidden`}>
+      <div className={`lg:flex items-center gap-10 hidden`}>
         <div>
           <Link href="/">
-            <p className="font-semibold py-[0.4rem] px-5 rounded-3xl text-white bg-[#0032F0]   cursor-pointer">
+            <p className="font-semibold py-[0.4rem] px-5 rounded-3xl text-white bg-[#0032F0] cursor-pointer">
               Home
             </p>
           </Link>
         </div>
-
         <div className="cursor-pointer">
           <Link href="#">
-            <p className="font-semibold text-[#1A2244] py-[0.4rem] px-5  ">
+            <p className="font-semibold text-[#1A2244] py-[0.4rem] px-5">
               Projects
             </p>
           </Link>
         </div>
         <div className="cursor-pointer">
           <Link href="#">
-            <p className="font-semibold text-[#1A2244] py-[0.4rem] px-5 ">
+            <p className="font-semibold text-[#1A2244] py-[0.4rem] px-5">
               Contact
             </p>
           </Link>
         </div>
         <div className="cursor-pointer">
           <Link href="#">
-            <p className="font-semibold text-[#1A2244] py-[0.4rem] px-5 ">
+            <p className="font-semibold text-[#1A2244] py-[0.4rem] px-5">
               About
             </p>
           </Link>
         </div>
         <div className="cursor-pointer">
           <Link href="#">
-            <p className="font-semibold text-[#1A2244] py-[0.4rem] px-5 ">
+            <p className="font-semibold text-[#1A2244] py-[0.4rem] px-5">
               FAQ
             </p>
           </Link>
