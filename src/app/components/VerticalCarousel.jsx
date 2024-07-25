cf09d75098a7c15c8ef04ac8a1ae9dda5b76bba6
Matthew Nweke <nwekematthew243@gmail.com>
2024-07-25 15:39:47 +0100

    all

--- src/app/components/VerticalCarousel.jsx
+++ src/app/components/VerticalCarousel.jsx
@@ -1,53 +1,11 @@
-import React, { useState, useEffect } from "react";
-import Image from "next/image";
-import styles from "../css/Styles.module.css"
+import React from 'react'
 
-const VerticalCarousel = ({ images }) => {
-  const [imageIndex, setImageIndex] = useState(0);
-  const [inTransition, setInTransition] = useState(false);
-  const images = ["/img_slide1.svg", "/img_slide2.svg", "/img_slide3.svg"];
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setInTransition(true);
-      setTimeout(() => {
-        setImageIndex((prevIndex) =>
-          prevIndex === images.length - 1 ? 0 : prevIndex + 1
-        );
-        setInTransition(false);
-      }, 1000); // transition duration
-    }, 3000); // interval between slides
-
-    return () => clearInterval(interval);
-  }, []);
-
+const Verticalcarousel = () => {
   return (
-    <div className={styles["carousel-container"]}>
-      <div
-        className={styles["carousel-wrapper"]}
-        style={{ transform: `translateY(-${imageIndex * 100}%)` }}
-      >
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className={`${styles["carousel-slide"]} ${
-              inTransition ? styles["slide-exit"] : styles["slide-enter"]
-            }`}
-          >
-            <Image
-              src={image}
-              alt={`Image Slide ${index}`}
-              layout="fill"
-              objectFit="contain"
-              loading="lazy"
-              className={
-                inTransition ? styles["slide-exit"] : styles["slide-enter"]
-              }
-            />
-          </div>
-        ))}
-      </div>
+    <div>
+      lorem
     </div>
-  );
-};
+  )
+}
 
-export default VerticalCarousel;
+export default Verticalcarousel
